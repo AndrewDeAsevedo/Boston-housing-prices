3cfb9157153be45a1a1f53a8b4b88b2d50c4e8fa
Ali <ali.hounain@gmail.com>
2024-10-22 16:48:16 -0400

    Added crime csv file, cleaned data csv even more

--- parse.py
+++ parse.py
@@ -4,10 +4,10 @@ import streamlit as st
 
 
 df = pd.read_csv("data.csv")
-#profile = ProfileReport(df, title="Pandas Profiling Report")
-# profile.to_notebook_iframe()
-#with open("report.html", "w") as f:
-   # f.write(profile.to_html())
+profile = ProfileReport(df, title="Pandas Profiling Report")
+profile.to_notebook_iframe()
+with open("report.html", "w") as f:
+   f.write(profile.to_html())
 pd.to_numeric(df["LAND_SF"], errors="coerce")
 pd.to_numeric(df["TOTAL_VALUE"], errors="coerce")
 df["LAND_SF"].corr(df["TOTAL_VALUE"])
