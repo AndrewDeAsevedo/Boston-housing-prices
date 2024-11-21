a876dbd72c05107eb8675a9da0169554c7e46f14
Andrew De Asevedo <andrewdeasevedo11@gmail.com>
2024-11-21 18:26:57 -0500

    moved table into its own tab

--- page/index.php
+++ page/index.php
@@ -92,59 +92,6 @@ $data = array_slice($data, $offset, $rowsPerPage);
         </div>
     </section>
 
-    <!-- Housing Data Table -->
-    <section class="housing-data">
-        <h3>Housing Data</h3>
-        <table>
-            <thead>
-                <tr>
-                    <th><a href="?sort=CITY&page=<?php echo $currentPage; ?>">City</a></th>
-                    <th><a href="?sort=ZIP_CODE&page=<?php echo $currentPage; ?>">ZIP Code</a></th>
-                    <th><a href="?sort=NUM_BLDGS&page=<?php echo $currentPage; ?>">Number of Buildings</a></th>
-                    <th><a href="?sort=LU_DESC&page=<?php echo $currentPage; ?>">Land Use Description</a></th>
-                    <th><a href="?sort=BLDG_TYPE&page=<?php echo $currentPage; ?>">Building Type</a></th>
-                    <th><a href="?sort=RES_FLOOR&page=<?php echo $currentPage; ?>">Residential Floor</a></th>
-                    <th><a href="?sort=LAND_SF&page=<?php echo $currentPage; ?>">Land SF</a></th>
-                    <th><a href="?sort=GROSS_AREA&page=<?php echo $currentPage; ?>">Gross Area</a></th>
-                    <th><a href="?sort=LIVING_AREA&page=<?php echo $currentPage; ?>">Living Area</a></th>
-                    <th><a href="?sort=LAND_VALUE&page=<?php echo $currentPage; ?>">Land Value</a></th>
-                    <th><a href="?sort=BLDG_VALUE&page=<?php echo $currentPage; ?>">Building Value</a></th>
-                    <th><a href="?sort=TOTAL_VALUE&page=<?php echo $currentPage; ?>">Total Value</a></th>
-                    <th><a href="?sort=GROSS_TAX&page=<?php echo $currentPage; ?>">Gross Tax</a></th>
-                    <th><a href="?sort=YR_BUILT&page=<?php echo $currentPage; ?>">Year Built</a></th>
-                </tr>
-            </thead>
-            <tbody>
-                <?php foreach ($data as $row): ?>
-                    <tr>
-                        <td><?php echo htmlspecialchars($row['CITY']); ?></td>
-                        <td><?php echo htmlspecialchars($row['ZIP_CODE']); ?></td>
-                        <td><?php echo htmlspecialchars($row['NUM_BLDGS']); ?></td>
-                        <td><?php echo htmlspecialchars($row['LU_DESC']); ?></td>
-                        <td><?php echo htmlspecialchars($row['BLDG_TYPE']); ?></td>
-                        <td><?php echo htmlspecialchars($row['RES_FLOOR']); ?></td>
-                        <td><?php echo htmlspecialchars($row['LAND_SF']); ?></td>
-                        <td><?php echo htmlspecialchars($row['GROSS_AREA']); ?></td>
-                        <td><?php echo htmlspecialchars($row['LIVING_AREA']); ?></td>
-                        <td><?php echo htmlspecialchars($row['LAND_VALUE']); ?></td>
-                        <td><?php echo htmlspecialchars($row['BLDG_VALUE']); ?></td>
-                        <td><?php echo htmlspecialchars($row['TOTAL_VALUE']); ?></td>
-                        <td><?php echo htmlspecialchars($row['GROSS_TAX']); ?></td>
-                        <td><?php echo htmlspecialchars($row['YR_BUILT']); ?></td>
-                    </tr>
-                <?php endforeach; ?>
-            </tbody>
-        </table>
-
-        <div class="pagination">
-            <p>Total Pages: <?php echo $totalPages; ?></p>
-            <form method="get">
-                <input type="number" name="page" min="1" max="<?php echo $totalPages; ?>" value="<?php echo $currentPage; ?>">
-                <input type="hidden" name="sort" value="<?php echo isset($sortField) ? $sortField : ''; ?>">
-                <input type="submit" value="Go">
-            </form>
-        </div>
-    </section>
 
     <!-- Footer Section -->
     <footer>
